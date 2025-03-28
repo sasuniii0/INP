b20ea1dc31e932029221d70cbcba94395a8e5b72
sasuniii0 <sasuniwijerathne@gmail.com>
2025-03-28 11:39:53 +0530

    first commit

---
+++ src/main/java/lk/ijse/gdse/Server.java
@@ -0,0 +1,14 @@
+package lk.ijse.gdse;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Server Starting....");
+
+        //server Socket
+
+        ServerSocket serverSocket = new ServerSocket(5000);
+    }
+}
