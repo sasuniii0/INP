4786f253b8aca5ea3161964edba0041e8a82429d
sasuniii0 <sasuniwijerathne@gmail.com>
2025-03-28 11:45:10 +0530

    Implement basic server functionality to accept client connections and read messages

--- src/main/java/lk/ijse/gdse/Server.java
+++ src/main/java/lk/ijse/gdse/Server.java
@@ -1,14 +1,31 @@
 package lk.ijse.gdse;
 
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
-        System.out.println("Server Starting....");
+    public static void main(String[] args){
 
-        //server Socket
+        try {
+            //Server Socket
+            System.out.println("Server Starting....");
+            ServerSocket serverSocket = new ServerSocket(5000);
 
-        ServerSocket serverSocket = new ServerSocket(5000);
+            //local Socket
+            Socket socket = serverSocket.accept();
+            System.out.println("Client Connected");
+
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            String message = in.readUTF();
+            System.out.println(message);
+
+            socket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
